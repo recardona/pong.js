5935ae8b3c896947dc401275ce930dd5c028ef6c
Rogelio E. Cardona-Rivera <me@rogel.io>
2012-08-28 21:17:47 -0600

    Added a simple Set class.  A Set is a collection with no duplicates.  Also added the prototype.js library.

---
+++ Set.js
@@ -0,0 +1,68 @@
+/**
+ * A Set is a collection that has no duplicate values.
+ */
+var Set = Class.create({
+   
+   initialize:function() {
+       this.rawArray = [];
+   },
+
+
+    /**
+     * Add an element only if it doesn't exist.
+     * @param {Object} the element to add
+     */
+    add:function(elem) {
+        if(this.contains(elem) == undefined){
+            this.rawArray.push(elem)
+        }
+    },
+    
+    
+    /**
+     * Returns the element at the given index. 
+     * @param {Number} index the index of the desired element
+     * @return {Object} the element at the index
+     */
+    get:function(index) {
+        return this.rawArray[index];
+    },
+    
+    
+    /**
+     * Remove an element only if it exists in the set. 
+     * @param {Object} elem the element to remove.
+     */
+    remove:function(elem) {
+        var index = this.contains(elem);
+        if(index != undefined)
+            this.rawArray.remove(index);
+    },
+    
+    
+    /**
+     * Returns the index of the element if it exists in the set.
+     * Otherwise, returns "undefined". 
+     * @param {Object} elem the element to search for
+     * @return {Number} the index of the element or undefined if
+     *          it does not exist in the set
+     */
+    contains:function(elem) {
+        for(var i = 0; i < this.rawArray.length; i++) {
+            var elem2 = this.rawArray[i];
+            if(elem.equals(elem2))
+                return i;
+        }
+        return undefined;
+    }    
+});
+
+
+/**
+ * Extends the Number to include an equals function, which
+ * tests the equality of 'this' against a parameter.
+ * @param {Object} num
+ */
+Number.prototype.equals = function(num) {
+    return this == num;
+}
